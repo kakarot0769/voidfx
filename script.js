88997bdc451720a642ac8ca9954ccbd40ff33350
Rahul Kumar Shaw <kakarotsucks07@gmail.com>
2026-08-19 21:16:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Replace these placeholder URLs with your real images
+    document.getElementById('logo-img').src = 'YOUR_LOGO_CIRCLE_IMAGE_URL_HERE';
+    document.getElementById('about-img').src = 'YOUR_ABOUT_PHOTO_URL_HERE';
+    document.getElementById('footer-img').src = 'YOUR_SMALL_CIRCLE_PHOTO_URL_HERE';
+    
